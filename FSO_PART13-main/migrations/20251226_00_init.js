d19513f1843b1d922147023338c16e385502d693
Robin Widjeskog <robin.widjeskog@gmail.com>
2026-01-05 01:42:18 +0200

    13.18

--- FSO_PART13-main/migrations/20251226_00_init.js
+++ FSO_PART13-main/migrations/20251226_00_init.js
@@ -2,6 +2,33 @@ const { DataTypes } = require("sequelize");
 
 module.exports = {
   up: async ({ context: queryInterface }) => {
+    await queryInterface.createTable("users", {
+      id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+
+      name: {
+        type: DataTypes.TEXT,
+
+        allowNull: false,
+      },
+      username: {
+        type: DataTypes.TEXT,
+
+        allowNull: false,
+        validate: {
+          isEmail: true,
+        },
+      },
+      created_at: {
+        type: DataTypes.DATE,
+      },
+      updated_at: {
+        type: DataTypes.DATE,
+      },
+    });
     await queryInterface.createTable("blogs", {
       id: {
         type: DataTypes.INTEGER,
@@ -23,38 +50,23 @@ module.exports = {
         type: DataTypes.INTEGER,
         defaultValue: 0,
       },
-      createdAt: {
+      created_at: {
         type: DataTypes.DATE,
+        allowNull: false,
+        defaultValue: DataTypes.NOW,
       },
-      updatedAt: {
+      updated_at: {
         type: DataTypes.DATE,
+        allowNull: false,
+        defaultValue: DataTypes.NOW,
       },
-    });
-    await queryInterface.createTable("users", {
-      id: {
+      user_id: {
         type: DataTypes.INTEGER,
-        primaryKey: true,
-        autoIncrement: true,
-      },
-
-      name: {
-        type: DataTypes.TEXT,
-
-        allowNull: false,
-      },
-      username: {
-        type: DataTypes.TEXT,
-
-        allowNull: false,
-        validate: {
-          isEmail: true,
+        allowNull: true,
+        references: {
+          model: "users",
+          key: "id",
         },
-      },
-      createdAt: {
-        type: DataTypes.DATE,
-      },
-      updatedAt: {
-        type: DataTypes.DATE,
       },
     });
   },
