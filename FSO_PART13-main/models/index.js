f5e8a4b218a70e0b736aa97a26e5038a43f1df49
Robin Widjeskog <robin.widjeskog@gmail.com>
2026-01-05 19:20:23 +0200

    13.19

--- FSO_PART13-main/models/index.js
+++ FSO_PART13-main/models/index.js
@@ -1,8 +1,12 @@
 const { Blog } = require("./blog");
 const { User } = require("./user");
+const { Readlist } = require("./readlist");
 
 User.hasMany(Blog);
 Blog.belongsTo(User);
+
+User.belongsToMany(Blog, { through: Readlist, as: "markedBlogs" });
+Blog.belongsToMany(User, { through: Readlist, as: "usersMarked" });
 
 module.exports = {
   Blog,
---
+++ FSO_PART13-main/models/readlist.js
@@ -0,0 +1,37 @@
+const { sequelize } = require("../util/db");
+const { Model, DataTypes } = require("sequelize");
+
+class Readlist extends Model {}
+Readlist.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    userId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: { model: "users", key: "id" },
+    },
+    blogId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: { model: "blogs", key: "id" },
+    },
+
+    read: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+      defaultValue: false,
+    },
+  },
+  {
+    sequelize,
+    underscored: true,
+    timestamps: true,
+    modelName: "readlist",
+  }
+);
+
+module.exports = { Readlist };
